ba32e18e6128973a72344b3633adf60100cd8405
Ami Halsey <cro19030@byui.edu>
2025-01-10 19:24:52 -0700

    Code written for prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your class grade? (In numbers) ");
+        string grade = Console.ReadLine();
+
+        int x = int.Parse(grade);
+        string letter = x.ToString();
+
+        if (x >= 90)
+        {
+            letter = "A";
+            // Console.WriteLine("Your grade is an A");
+        }
+        else if (x < 90 && x >= 80)
+        {
+            letter = "B";
+            // Console.WriteLine("Your grade is a B");
+        }
+        else if (x < 80 && x >= 70)
+        {
+            letter = "C";
+            // Console.WriteLine("Your grade is a C");
+        }
+        else if (x < 70 && x >= 60)
+        {
+            letter = "D";
+            // Console.WriteLine("Your grade is a D");
+        }
+        else
+        {
+            letter = "F";
+            // Console.WriteLine("Your grade is an F");
+        }
+
+        Console.WriteLine(letter);
+        if (x > 70)
+        {
+            Console.WriteLine("You passed, Congratulation!");
+        }
+        else
+        {
+            Console.WriteLine("You failed, better luck next time!");
+        }
     }
 }
