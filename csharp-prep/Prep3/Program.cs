016ec01b1e4235f53cacce7fae6dce2ce9b57935
Ami Halsey <cro19030@byui.edu>
2025-01-14 21:29:29 -0700

    random number generator game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is your magic number? ");
+        // string magicNum = Console.ReadLine();
+        // int x = int.Parse(magicNum);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        Console.Write("Please enter your number guess: ");
+        string guess = Console.ReadLine();
+        int y = int.Parse(guess);
+
+        if (y < number)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (y > number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it right!");
+        }
+
+        do
+        {
+            Console.Write("Please enter your number guess: ");
+            string tryAgain = Console.ReadLine();
+            y = int.Parse(tryAgain);
+
+            if (y < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (y > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it right!");
+            }
+        }
+        while (y != number);
+
     }
 }
