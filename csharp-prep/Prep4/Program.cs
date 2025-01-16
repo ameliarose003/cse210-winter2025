20aeb373b5602cb862f1c69254b22be1e8c37ecb
Ami Halsey <cro19030@byui.edu>
2025-01-15 21:29:05 -0700

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers;
+        numbers = new List<int>();
+        int sum = 0;
+        int average = 0;
+        int? largest = null;
+        int? smallest = null;
+
+
+        int num = -1;
+        do
+        {
+            Console.Write("Please enter a number: ");
+            string input = Console.ReadLine();
+            num = int.Parse(input);
+            if (num != 0)
+            {
+                numbers.Add(num);
+            }
+        }
+        while (num != 0);
+
+        Console.WriteLine(numbers.Count + $"numbers were entered.");
+        int numCount = numbers.Count;
+
+        foreach (int number in numbers)
+        {
+            // Console.WriteLine(number);
+            sum += number;
+
+            if (largest == null || number > largest)
+            {
+                largest = number;
+            }
+            else if (smallest == null || number < smallest && number != 0)
+            {
+                smallest = number;
+            }
+            else
+            {
+                Console.WriteLine("Please input valid numbers or 0 to finish. ");
+            }
+
+
+        }
+
+        Console.WriteLine($"The sum is: " + sum);
+        average = sum / (numCount - 1);
+        Console.WriteLine($"The average is: " + average);
+
+        if (largest.HasValue && smallest.HasValue)
+        {
+            Console.WriteLine($"The largest numbers is: " + largest);
+            Console.WriteLine($"The smallest numbers is: " + smallest);
+        }
+        else
+        {
+            Console.WriteLine("No number was entered");
+        }
     }
 }
